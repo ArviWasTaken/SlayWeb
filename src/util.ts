f0a6e9c1693030a2a1d9c990d31aa1a31e09008d
Arvid Smit <arvid.code@freedom.nl>
2024-11-10 21:33:36 +0100

    Removed unused methods

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,3 @@
 export function randomInteger(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-export function randomOrientation() {
-    return randomInteger(0, 360);
-}
-
-export function pythagoras(a: number, b: number): number {
-    return Math.sqrt(a * a + b * b);
-}
--- src/vector.ts
+++ src/vector.ts
@@ -16,40 +16,4 @@ export class Vector {
     add(other: Vector) {
         return new Vector(this.x + other.x, this.y + other.y);
     }
-
-    multiply(multiplier: number): Vector {
-        return new Vector(this.x * multiplier, this.y * multiplier);
-    }
-
-    findAngleTowards(other: Vector) {
-        return Math.atan2(other.y - this.y, other.x - this.x) * 180 / Math.PI;
-    }
-
-    findDistanceBetween(other: Vector) {
-        return Math.sqrt(Math.pow(other.x - this.x, 2) - Math.pow(other.y + this.y, 2));
-    }
-
-    findPointInDirection(angle: number, distance: number): Vector {
-        const radianAngle = angle * Math.PI / 180;
-        return new Vector(
-            this.x + (distance * Math.cos(radianAngle)),
-            this.y + (distance * Math.sin(radianAngle)),
-        );
-    }
-
-    static random(xmin: number, xmax: number, ymin?: number, ymax?: number): Vector {
-        if (ymin == undefined || ymax == undefined) {
-            return new Vector(
-                randomInteger(xmin, xmax),
-                randomInteger(xmin, xmax),
-            );
-        } else {
-            return new Vector(
-                randomInteger(xmin, xmax),
-                randomInteger(ymin, ymax),
-            );
-        }
-
-    }
-
 }
